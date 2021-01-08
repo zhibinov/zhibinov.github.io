a5b5562ac09f2d4f8bdde82dfb452ebe5afab963
zhibinov <zhibinov.pavel@gmail.com>
2021-01-08 22:42:40 +0300

    правки

--- u-serviceNEW/script.js
+++ u-serviceNEW/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let navBox = document.querySelector('.b-nav')
 let navLink1 = document.querySelector('.b-nav__link_1');
 let navOptions1 = document.querySelector('.b-nav__options_1');
 let navLink2 = document.querySelector('.b-nav__link_2');
@@ -9,34 +10,300 @@ let navOptions3 = document.querySelector('.b-nav__options_3');
 let navLink4 = document.querySelector('.b-nav__link_4');
 let navOptions4 = document.querySelector('.b-nav__options_4');
 
-navLink1.addEventListener('click', function (event) {
-    navOptions1.classList.toggle('b-nav__options_block');
+navBox.addEventListener('mouseout', function (event) {
+    navOptions1.classList.remove('b-nav__options_block');
     navOptions2.classList.remove('b-nav__options_block');
     navOptions3.classList.remove('b-nav__options_block');
     navOptions4.classList.remove('b-nav__options_block');
 });
 
-navLink2.addEventListener('click', function (event) {
-    navOptions1.classList.remove('b-nav__options_block');
-    navOptions2.classList.toggle('b-nav__options_block');
+navLink1.addEventListener('mouseover', function (event) {
+    navOptions1.classList.add('b-nav__options_block');
+    navOptions2.classList.remove('b-nav__options_block');
     navOptions3.classList.remove('b-nav__options_block');
     navOptions4.classList.remove('b-nav__options_block');
 });
 
-navLink3.addEventListener('click', function (event) {
+navLink2.addEventListener('mouseover', function (event) {
     navOptions1.classList.remove('b-nav__options_block');
-    navOptions2.classList.remove('b-nav__options_block');
-    navOptions3.classList.toggle('b-nav__options_block');
+    navOptions2.classList.add('b-nav__options_block');
+    navOptions3.classList.remove('b-nav__options_block');
     navOptions4.classList.remove('b-nav__options_block');
 });
 
-navLink4.addEventListener('click', function (event) {
+navLink3.addEventListener('mouseover', function (event) {
+    navOptions1.classList.remove('b-nav__options_block');
+    navOptions2.classList.remove('b-nav__options_block');
+    navOptions3.classList.add('b-nav__options_block');
+    navOptions4.classList.remove('b-nav__options_block');
+});
+
+navLink4.addEventListener('mouseover', function (event) {
     navOptions1.classList.remove('b-nav__options_block');
     navOptions2.classList.remove('b-nav__options_block');
     navOptions3.classList.remove('b-nav__options_block');
-    navOptions4.classList.toggle('b-nav__options_block');
+    navOptions4.classList.add('b-nav__options_block');
 });
 
+let burger = document.querySelector('.b-header__burger');
+let navbar = document.querySelector('.b-nav-740')
+burger.addEventListener('click', function (event) {
+    navbar.classList.toggle('display_block');
+});
+
+let buttonApple = document.querySelector('.b-nav-740__button_apple');
+let buttonPhone = document.querySelector('.b-nav-740__button_phone');
+let buttonNote = document.querySelector('.b-nav-740__button_note');
+let buttonTable = document.querySelector('.b-nav-740__button_table');
+
+let level2Apple = document.querySelector('.b-nav-740__2level-apple');
+let level2Phone = document.querySelector('.b-nav-740__2level-phone');
+let level2Note = document.querySelector('.b-nav-740__2level-note');
+let level2Table = document.querySelector('.b-nav-740__2level-table');
+
+buttonApple.addEventListener('click', function (event) {
+    level2Apple.classList.toggle('display_block');
+    level2Phone.classList.remove('display_block');
+    level2Note.classList.remove('display_block');
+    level2Table.classList.remove('display_block');
+});
+
+buttonPhone.addEventListener('click', function (event) {
+    level2Apple.classList.remove('display_block');
+    level2Phone.classList.toggle('display_block');
+    level2Note.classList.remove('display_block');
+    level2Table.classList.remove('display_block');
+});
+
+buttonNote.addEventListener('click', function (event) {
+    level2Apple.classList.remove('display_block');
+    level2Phone.classList.remove('display_block');
+    level2Note.classList.toggle('display_block');
+    level2Table.classList.remove('display_block');
+});
+
+buttonTable.addEventListener('click', function (event) {
+    level2Apple.classList.remove('display_block');
+    level2Phone.classList.remove('display_block');
+    level2Note.classList.remove('display_block');
+    level2Table.classList.toggle('display_block');
+});
+
+
+let button2Apple1 = document.querySelector('.b-nav-740__button-apple1');
+let button2Apple2 = document.querySelector('.b-nav-740__button-apple2');
+let button2Apple3 = document.querySelector('.b-nav-740__button-apple3');
+let button2Apple4 = document.querySelector('.b-nav-740__button-apple4');
+let button2Apple5 = document.querySelector('.b-nav-740__button-apple5');
+
+let button2Phone1 = document.querySelector('.b-nav-740__button-phone1');
+let button2Phone2 = document.querySelector('.b-nav-740__button-phone2');
+let button2Phone3 = document.querySelector('.b-nav-740__button-phone3');
+let button2Phone4 = document.querySelector('.b-nav-740__button-phone4');
+let button2Phone5 = document.querySelector('.b-nav-740__button-phone5');
+
+let button2Note1 = document.querySelector('.b-nav-740__button-note1');
+let button2Note2 = document.querySelector('.b-nav-740__button-note2');
+let button2Note3 = document.querySelector('.b-nav-740__button-note3');
+let button2Note4 = document.querySelector('.b-nav-740__button-note4');
+let button2Note5 = document.querySelector('.b-nav-740__button-note5');
+
+let button2Table1 = document.querySelector('.b-nav-740__button-table1');
+let button2Table2 = document.querySelector('.b-nav-740__button-table2');
+let button2Table3 = document.querySelector('.b-nav-740__button-table3');
+let button2Table4 = document.querySelector('.b-nav-740__button-table4');
+let button2Table5 = document.querySelector('.b-nav-740__button-table5');
+
+
+let nav3Apple1 = document.querySelector('.b-nav-740__3level-apple1');
+let nav3Apple2 = document.querySelector('.b-nav-740__3level-apple2');
+let nav3Apple3 = document.querySelector('.b-nav-740__3level-apple3');
+let nav3Apple4 = document.querySelector('.b-nav-740__3level-apple4');
+let nav3Apple5 = document.querySelector('.b-nav-740__3level-apple5');
+
+let nav3Phone1 = document.querySelector('.b-nav-740__3level-phone1');
+let nav3Phone2 = document.querySelector('.b-nav-740__3level-phone2');
+let nav3Phone3 = document.querySelector('.b-nav-740__3level-phone3');
+let nav3Phone4 = document.querySelector('.b-nav-740__3level-phone4');
+let nav3Phone5 = document.querySelector('.b-nav-740__3level-phone5');
+
+let nav3Note1 = document.querySelector('.b-nav-740__3level-note1');
+let nav3Note2 = document.querySelector('.b-nav-740__3level-note2');
+let nav3Note3 = document.querySelector('.b-nav-740__3level-note3');
+let nav3Note4 = document.querySelector('.b-nav-740__3level-note4');
+let nav3Note5 = document.querySelector('.b-nav-740__3level-note5');
+
+let nav3Table1 = document.querySelector('.b-nav-740__3level-table1');
+let nav3Table2 = document.querySelector('.b-nav-740__3level-table2');
+let nav3Table3 = document.querySelector('.b-nav-740__3level-table3');
+let nav3Table4 = document.querySelector('.b-nav-740__3level-table4');
+let nav3Table5 = document.querySelector('.b-nav-740__3level-table5');
+
+button2Apple1.addEventListener('click', function (event) {
+    nav3Apple1.classList.toggle('display_block');
+    nav3Apple2.classList.remove('display_block');
+    nav3Apple3.classList.remove('display_block');
+    nav3Apple4.classList.remove('display_block');
+    nav3Apple5.classList.remove('display_block');
+});
+
+button2Apple2.addEventListener('click', function (event) {
+    nav3Apple1.classList.remove('display_block');
+    nav3Apple2.classList.toggle('display_block');
+    nav3Apple3.classList.remove('display_block');
+    nav3Apple4.classList.remove('display_block');
+    nav3Apple5.classList.remove('display_block');
+});
+
+button2Apple3.addEventListener('click', function (event) {
+    nav3Apple1.classList.remove('display_block');
+    nav3Apple2.classList.remove('display_block');
+    nav3Apple3.classList.toggle('display_block');
+    nav3Apple4.classList.remove('display_block');
+    nav3Apple5.classList.remove('display_block');
+});
+
+button2Apple4.addEventListener('click', function (event) {
+    nav3Apple1.classList.remove('display_block');
+    nav3Apple2.classList.remove('display_block');
+    nav3Apple3.classList.remove('display_block');
+    nav3Apple4.classList.toggle('display_block');
+    nav3Apple5.classList.remove('display_block');
+});
+
+button2Apple5.addEventListener('click', function (event) {
+    nav3Apple1.classList.remove('display_block');
+    nav3Apple2.classList.remove('display_block');
+    nav3Apple3.classList.remove('display_block');
+    nav3Apple4.classList.remove('display_block');
+    nav3Apple5.classList.toggle('display_block');
+});
+
+//phone
+
+button2Phone1.addEventListener('click', function (event) {
+    nav3Phone1.classList.toggle('display_block');
+    nav3Phone2.classList.remove('display_block');
+    nav3Phone3.classList.remove('display_block');
+    nav3Phone4.classList.remove('display_block');
+    nav3Phone5.classList.remove('display_block');
+});
+
+button2Phone2.addEventListener('click', function (event) {
+    nav3Phone1.classList.remove('display_block');
+    nav3Phone2.classList.toggle('display_block');
+    nav3Phone3.classList.remove('display_block');
+    nav3Phone4.classList.remove('display_block');
+    nav3Phone5.classList.remove('display_block');
+});
+
+button2Phone3.addEventListener('click', function (event) {
+    nav3Phone1.classList.remove('display_block');
+    nav3Phone2.classList.remove('display_block');
+    nav3Phone3.classList.toggle('display_block');
+    nav3Phone4.classList.remove('display_block');
+    nav3Phone5.classList.remove('display_block');
+});
+
+button2Phone4.addEventListener('click', function (event) {
+    nav3Phone1.classList.remove('display_block');
+    nav3Phone2.classList.remove('display_block');
+    nav3Phone3.classList.remove('display_block');
+    nav3Phone4.classList.toggle('display_block');
+    nav3Phone5.classList.remove('display_block');
+});
+
+button2Phone5.addEventListener('click', function (event) {
+    nav3Phone1.classList.remove('display_block');
+    nav3Phone2.classList.remove('display_block');
+    nav3Phone3.classList.remove('display_block');
+    nav3Phone4.classList.remove('display_block');
+    nav3Phone5.classList.toggle('display_block');
+});
+
+//note
+
+button2Note1.addEventListener('click', function (event) {
+    nav3Note1.classList.toggle('display_block');
+    nav3Note2.classList.remove('display_block');
+    nav3Note3.classList.remove('display_block');
+    nav3Note4.classList.remove('display_block');
+    nav3Note5.classList.remove('display_block');
+});
+
+button2Note2.addEventListener('click', function (event) {
+    nav3Note1.classList.remove('display_block');
+    nav3Note2.classList.toggle('display_block');
+    nav3Note3.classList.remove('display_block');
+    nav3Note4.classList.remove('display_block');
+    nav3Note5.classList.remove('display_block');
+});
+
+button2Note3.addEventListener('click', function (event) {
+    nav3Note1.classList.remove('display_block');
+    nav3Note2.classList.remove('display_block');
+    nav3Note3.classList.toggle('display_block');
+    nav3Note4.classList.remove('display_block');
+    nav3Note5.classList.remove('display_block');
+});
+
+button2Note4.addEventListener('click', function (event) {
+    nav3Note1.classList.remove('display_block');
+    nav3Note2.classList.remove('display_block');
+    nav3Note3.classList.remove('display_block');
+    nav3Note4.classList.toggle('display_block');
+    nav3Note5.classList.remove('display_block');
+});
+
+button2Note5.addEventListener('click', function (event) {
+    nav3Note1.classList.remove('display_block');
+    nav3Note2.classList.remove('display_block');
+    nav3Note3.classList.remove('display_block');
+    nav3Note4.classList.remove('display_block');
+    nav3Note5.classList.toggle('display_block');
+});
+
+//table
+
+button2Table1.addEventListener('click', function (event) {
+    nav3Table1.classList.toggle('display_block');
+    nav3Table2.classList.remove('display_block');
+    nav3Table3.classList.remove('display_block');
+    nav3Table4.classList.remove('display_block');
+    nav3Table5.classList.remove('display_block');
+});
+
+button2Table2.addEventListener('click', function (event) {
+    nav3Table1.classList.remove('display_block');
+    nav3Table2.classList.toggle('display_block');
+    nav3Table3.classList.remove('display_block');
+    nav3Table4.classList.remove('display_block');
+    nav3Table5.classList.remove('display_block');
+});
+
+button2Table3.addEventListener('click', function (event) {
+    nav3Table1.classList.remove('display_block');
+    nav3Table2.classList.remove('display_block');
+    nav3Table3.classList.toggle('display_block');
+    nav3Table4.classList.remove('display_block');
+    nav3Table5.classList.remove('display_block');
+});
+
+button2Table4.addEventListener('click', function (event) {
+    nav3Table1.classList.remove('display_block');
+    nav3Table2.classList.remove('display_block');
+    nav3Table3.classList.remove('display_block');
+    nav3Table4.classList.toggle('display_block');
+    nav3Table5.classList.remove('display_block');
+});
+
+button2Table5.addEventListener('click', function (event) {
+    nav3Table1.classList.remove('display_block');
+    nav3Table2.classList.remove('display_block');
+    nav3Table3.classList.remove('display_block');
+    nav3Table4.classList.remove('display_block');
+    nav3Table5.classList.toggle('display_block');
+});
 
 let raitingY = document.querySelector('.b-review__raiting-card_y');
 let sliderY = document.querySelector('.b-review__slider_y');
@@ -73,12 +340,6 @@ raitingI.addEventListener('click', function (event) {
     sliderG.classList.remove('b-review__slider_b');
     slider2.classList.remove('b-review__slider_b');
     sliderI.classList.add('b-review__slider_b');
-});
-
-let burger = document.querySelector('.b-header__burger');
-let navbar = document.querySelector('.b-nav-box')
-burger.addEventListener('click', function (event) {
-    navbar.classList.toggle('b-nav__options_block');
 });
 
 let close = document.querySelector('.b-pop-up__header-button');
