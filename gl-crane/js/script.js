5861f43d59de53578e29b0bf73c72f95b64cc28c
zhibinov <zhibinov.pavel@gmail.com>
2021-02-18 14:14:29 +0300

    адаптив

--- gl-crane/js/script.js
+++ gl-crane/js/script.js
@@ -3,31 +3,38 @@
 const burgerButton = document.querySelector('.b-header__burger');
 const burgerMenu = document.querySelector('.b-header__burger-menu');
 const burgerClose = document.querySelector('.b-header__close-button');
+const body = document.querySelector('body');
 
-function openMenu() {
-    if (burgerMenu.classList.contains("open")) {
-        burgerMenu.classList.remove("open");
-    } else {
-        burgerMenu.classList.add("open");
-        burgerMenu.classList.add("shadow");
+const toggleMenu = () => {
+    burgerMenu.classList.toggle('open');
+    burgerMenu.classList.toggle("shadow");
+    body.classList.toggle("scroll");
+}
+
+burgerClose.addEventListener('click', e => {
+    e.stopPropagation();
+    toggleMenu();
+});
+
+burgerButton.addEventListener('click', e => {
+    e.stopPropagation();
+    toggleMenu();
+});
+
+document.addEventListener('click', e => {
+    let target = e.target;
+    let its_burgerMenu = target == burgerMenu || burgerMenu.contains(target);
+    let its_burgerButton = target == burgerButton;
+    let burgerMenu_is_open = burgerMenu.classList.contains('open');
+
+    if (!its_burgerMenu && !its_burgerButton && burgerMenu_is_open) {
+        toggleMenu();
     }
-}
-
-function closeMenu() {
-    burgerMenu.classList.remove("open");
-    burgerMenu.classList.remove("shadow");
-}
+})
 
 var tel = document.getElementsByClassName('imaskjs__input_tel');
 for (var i = 0; i < tel.length; i++) {
     new IMask(tel[i], {
-        mask: '+{7} 000 - 000 - 00 - 00',
-    });
-}
-
-var tel1 = document.getElementsByClassName('imaskjs__input_tel1');
-for (var i = 0; i < tel1.length; i++) {
-    new IMask(tel1[i], {
         mask: '+{7} 000 - 000 - 00 - 00',
     });
 }
