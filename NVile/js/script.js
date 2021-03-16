b2cc978f2610f436123de8f129f958c88fdcc2b7
zhibinov <zhibinov.pavel@gmail.com>
2021-03-16 13:32:44 +0300

    nvile

---
+++ NVile/js/script.js
@@ -0,0 +1,25 @@
+const copyLink = (e) => {
+    e.preventDefault();
+    let aux = document.createElement('input');
+    let url = e.target.getAttribute('href');
+    aux.setAttribute('value', url);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand('copy');
+    document.body.removeChild(aux);
+};
+
+let link = document.querySelector('.b-link__link');
+link.addEventListener('click', copyLink);
+
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
