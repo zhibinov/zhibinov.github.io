d3c1467d11d7bae4d1591111254a800b8caac001
zhibinov <zhibinov.pavel@gmail.com>
2021-03-24 19:11:10 +0300

    restart_index

---
+++ ReStart/js/script.js
@@ -0,0 +1,63 @@
+var details = document.querySelectorAll(".b-review__button");
+
+for (i = 0; i < details.length; i++) {
+    details[i].addEventListener("toggle", accordion);
+}
+
+details[0].setAttribute('open', '');
+
+function accordion(event) {
+    if (!event.target.open) return;
+    var details = event.target.parentNode.children;
+
+    for (i = 0; i < details.length; i++) {
+        if (details[i].tagName != "DETAILS" || !details[i].hasAttribute('open') || event.target == details[i]) continue;
+        details[i].removeAttribute("open");
+    }
+}
+
+var detailsMap = document.querySelectorAll(".b-map__details");
+
+for (i = 0; i < detailsMap.length; i++) {
+    detailsMap[i].addEventListener("toggle", accordion);
+}
+
+detailsMap[0].setAttribute('open', '');
+
+function accordion(event) {
+    if (!event.target.open) return;
+    var detailsMap = event.target.parentNode.children;
+
+    for (i = 0; i < detailsMap.length; i++) {
+        if (detailsMap[i].tagName != "DETAILS" || !detailsMap[i].hasAttribute('open') || event.target == detailsMap[i]) continue;
+        detailsMap[i].removeAttribute("open");
+    }
+}
+
+var detailsNav = document.querySelectorAll(".b-nav-740__details");
+
+for (i = 0; i < detailsNav.length; i++) {
+    detailsNav[i].addEventListener("toggle", accordion);
+}
+
+function accordion(event) {
+    if (!event.target.open) return;
+    var detailsNav = event.target.parentNode.children;
+
+    for (i = 0; i < detailsNav.length; i++) {
+        if (detailsNav[i].tagName != "DETAILS" || !detailsNav[i].hasAttribute('open') || event.target == detailsNav[i]) continue;
+        detailsNav[i].removeAttribute("open");
+    }
+}
+
+let burgerButton = document.querySelector('.b-top__burger-button');
+let closeButton = document.querySelector('.b-burger-menu__close');
+let burgerMenu = document.querySelector('.b-burger-menu');
+
+burgerButton.addEventListener('click', function () {
+    burgerMenu.classList.toggle('display_block');
+})
+
+closeButton.addEventListener('click', function () {
+    burgerMenu.classList.toggle('display_block');
+})
