69a2f564efde2265e3cdd169f128dbd4e7c2d6fc
zhibinov <zhibinov.pavel@gmail.com>
2021-04-13 10:37:31 +0300

    карта

--- ReStart/js/script.js
+++ ReStart/js/script.js
@@ -61,3 +61,95 @@ burgerButton.addEventListener('click', function () {
 closeButton.addEventListener('click', function () {
     burgerMenu.classList.toggle('display_block');
 })
+
+let openCallBack = document.querySelector('.b-header__button');
+let callBack = document.querySelector('.callback');
+let closeCallBack = document.querySelector('.close_callback')
+
+openCallBack.addEventListener('click', function (event) {
+    callBack.style.display = 'block';
+});
+
+closeCallBack.addEventListener('click', function (event) {
+    callBack.style.display = 'none';
+});
+
+// let select = document.querySelector('.b-top__select_1');
+// let selectPhone = document.querySelector('.b-top__select-box_phone');
+// let selectTablet = document.querySelector('.b-top__select-box_phone');
+// let selectNotebook = document.querySelector('.b-top__select-box_phone');
+// let selectPC = document.querySelector('.b-top__select-box_phone');
+// let selectTech = document.querySelector('.b-top__select-box_phone');
+
+// function changeInput1(){
+// if (select.options.selectedIndex === 1){
+//     selectPhone.classList.toggle('display_block');
+// }else if(select.options.selectedIndex === 2){
+//     selectPhone.classList.toggle('display_block');
+// }
+
+// }
+
+// console.log(select.options.selectedIndex);
+
+// get stuff from the DOM
+// var regsel = document.getElementById("regions");
+// var select = document.getElementById("countries");
+// var models = document.getElementById("models");
+
+// function countryMethod(name, region) {
+//    this.name = name;
+//    this.region = region;
+// }
+
+// function modelsMethod(name, country) {
+//     this.name = name;
+//     this.country = country;
+//  }
+
+// var africa = ["Botswana", "Cameroon", "Ghana", "Kenya"];
+// var americas = ["Canada"];
+// var eurasia = ["United Kingdom"];
+// var pacific = ["Australia", "Fiji", "Kiribati", "Nauru", "New Zealand"];
+// var all = [0, africa, americas, eurasia, pacific];
+
+
+
+// function populateSelect(params) {
+
+//    while (select.firstChild) {
+//       select.removeChild(select.firstChild);
+//    };
+
+//    if (params > 0) {
+//       /*alert(params);*/
+//       var x = all[params];
+//       var a = 0;
+//       for (a = 0; a < x.length; a++) {
+//          var country = x[a];
+//          var el = document.createElement("option");
+//          el.textContent = country;
+//          el.value = country;
+//          select.appendChild(el);
+//       };
+//    }
+//    else {
+
+//       var m = [];
+//       m = m.concat(pacific, africa, eurasia, americas);
+//       m.sort();
+//       m.unshift("Выбор бренда");
+
+//       for (i = 0; i < m.length; i++) {
+//          var x = m[i];
+//          var el = document.createElement("option");
+//          el.textContent = x;
+//          el.value = x;
+//          select.appendChild(el);
+//       }
+
+//    } // end if
+
+// }
+
+// window.onload = populateSelect(0);
