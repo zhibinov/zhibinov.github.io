d9f26887d0d7412804b54aeab3d4d54c0a07c791
zhibinov <zhibinov.pavel@gmail.com>
2021-05-01 16:50:05 +0300

    о2

--- legal_orange/js/script.js
+++ legal_orange/js/script.js
@@ -32,3 +32,15 @@ let timer = () => {
 let i = setInterval(timer, 450);
 timer();
 
+let closePopUp = document.querySelector('.b-pop-up__close');
+let openPopUp = document.querySelectorAll('.open_pop-up');
+let popUp = document.querySelector('.b-pop-up');
+openPopUp.forEach(element => linkUp(element));
+function linkUp(item) {
+    item.addEventListener('click', function (event) {
+        popUp.style.display = 'flex';
+    });
+}
+closePopUp.addEventListener('click', function (event) {
+    popUp.style.display = 'none';
+});
