cb648f978ec4189797d422b7646fa8d7c3c17147
zhibinov <zhibinov.pavel@gmail.com>
2021-06-03 12:53:04 +0300

    i

--- institute/js/blog.js
+++ institute/js/blog.js
@@ -1,26 +1,3 @@
-const swiperReviews = new Swiper('.swiper-container_partners', {
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 5000,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        1200: {
-            slidesPerView: 4,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        576: {
-            slidesPerView: 2,
-        }
-    }
-})
-
 const burger = document.querySelector('.b-header-mobile__button');
 const burgerMenu = document.querySelector('.b-header-mobile__burger-menu');
 burger.addEventListener('click', function(){
