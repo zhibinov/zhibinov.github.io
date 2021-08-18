6fa1ee64b41d190e47755e17c93aa81c3664e578
zhibinov <zhibinov.pavel@gmail.com>
2021-08-18 10:55:57 +0300

    7.3

--- MegaMC/js/script.js
+++ MegaMC/js/script.js
@@ -34,13 +34,21 @@ languageEng.addEventListener('click', function (event) {
     languageImg.src = 'img/eng.svg'
 });
 
-$( function() {
-    $( ".b-main__input-cal" ).datepicker({ dateFormat: 'mm/dd/yy' }).datepicker("setDate", new Date()); 
-} );
+const d1 = $( ".b-main__input-cal" ).datepicker({
+	dateFormat: 'mm/dd/yy', 
+	onSelect: function(dateText){
+  	console.log(dateText)
+    const setRes = new Date(dateText);
+    setRes.setDate(setRes.getDate()+2)
+    
+    
+    console.log(setRes);
+    $( ".b-main__input-cal2" ).datepicker("setDate", setRes);
+  }
+})
+const d2 = $( ".b-main__input-cal2" ).datepicker({ dateFormat: 'mm/dd/yy' }).datepicker("setDate", 2);
 
-$( function() {
-    $( ".b-main__input-cal2" ).datepicker({ dateFormat: 'mm/dd/yy' }).datepicker("setDate", 2); 
-} );
+d1.datepicker("setDate", new Date()); 
 
 var elements = document.getElementsByClassName('b-main__time');
 for (var i = 0; i < elements.length; i++) {
